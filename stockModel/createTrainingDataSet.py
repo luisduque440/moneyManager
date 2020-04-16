f56a02445ef0e7a3ff75bee613f5c697ba2eef61
Luis Duque <lduque@indeed.com>
2020-04-16 18:00:35 -0500

    refactored a bit this code, its quite cool

--- stockModel/createTrainingDataSet.py
+++ stockModel/createTrainingDataSet.py
@@ -2,54 +2,56 @@ import pandas as pd
 from datetime import timedelta
 from loadData.loadTimeSeries import loadTimeSeries
 
+
 def createTrainingDataSet(stock, numSamples, currentTime, pastLen, futureLen):
     """ document
     """
-    assert pastLen>0, 'pastLen must be positive'
-    assert futureLen>0, 'futureLen must be positive'
-    dt = loadTimeSeries(stock, numSamples, currentTime)
-    dt['date']=dt.index
-    X = _createFeaturesFromTimeSeries(dt, pastLen)
-    y = _createTargetFromTimeSeries(dt, futureLen)
-    X, y = _cleanTrainingData(X, y)
+    X = _createFeatures(stock, numSamples, currentTime, pastLen)
+    y = _createTarget(stock, numSamples, currentTime, futureLen)
+    X, y = _keepRowsWithoutNaT(X), _keepRowsWithoutNaT(y)
+    X, y = _keepIndexesInBoth(X, y)
     return X, y
+
 
 def createFeaturesAtCurrentTime(stock, currentTime, pastLen):
     """ document
     """
-    dt = loadTimeSeries(stock, pastLen, currentTime)
-    dt['date']=dt.index
-    X = _createFeaturesFromTimeSeries(dt, pastLen)
-    X = _cleanFeatures(X)
+    X = _createFeatures(stock, pastLen, currentTime, pastLen)
+    X = _keepRowsWithoutNaT(X)
     if len(X)!=1: return None
     lastDateInFeatures = X.date.values[0][-1]
     if lastDateInFeatures!=(currentTime-timedelta(minutes=1)):
         return None
     return X
 
-_hasNulls= lambda x: len([t for t in x if pd.isnull(t)])>0
-_minutes = lambda x: (x[-1] - x[0]).seconds//60
-_correctLen = lambda x: False if _hasNulls(x) else (_minutes(x)== len(x) - 1)
 
-def _cleanFeatures(X):
-    validRows = X.date.apply(_correctLen)
-    X = X.loc[validRows].copy()
-    return X
+def _keepRowsWithoutNaT(df):
+    hasNulls = lambda x: len([t for t in x if pd.isnull(t)]) > 0
+    minutes = lambda x: (x[-1] - x[0]).seconds // 60
+    correctLen = lambda x: False if hasNulls(x) else (minutes(x) == len(x) - 1)
+    validRows = df.date.apply(correctLen)
+    df = df.loc[validRows].copy()
+    return df
 
-def _cleanTrainingData(X,y):
-    validRowsX = X.date.apply(_correctLen)
-    validRowsY = y.date.apply(_correctLen)
-    validRows= validRowsX & validRowsY
-    X, y = X.loc[validRows], y.loc[validRows]
-    return X, y
 
-def _createFeaturesFromTimeSeries(dt, pastLen):
+def _keepIndexesInBoth(X,y):
+    idx = X.index.intersection(y.index)
+    return X.loc[idx].copy(), y.loc[idx].copy()
+
+
+def _createFeatures(stock, numSamples, currentTime, pastLen):
+    assert pastLen > 0, 'pastLen must be positive'
+    dt = loadTimeSeries(stock, numSamples, currentTime)
+    dt['date'] = dt.index
     X = _pivotWindow(dt, pastLen-1, -1, dt.columns)
     lastFeatureDatePlusOneMinute = X.date.apply(lambda x: x[-1]).apply(lambda x:x+timedelta(minutes=1))
     X.index = lastFeatureDatePlusOneMinute
     return X
 
-def _createTargetFromTimeSeries(dt, futureLen):
+def _createTarget(stock, numSamples, currentTime, futureLen):
+    assert futureLen > 0, 'futureLen must be positive'
+    dt = loadTimeSeries(stock, numSamples, currentTime)
+    dt['date'] = dt.index
     df = _pivotWindow(dt, -1, -futureLen-1, dt.columns)
     nextMinuteValue =  df.consolidated.apply(lambda x:x[0])
     futureValue = df.consolidated.apply(lambda x:x[-1])
@@ -67,6 +69,7 @@ def _pivotWindow(ds, start, end, columnsToPivot):
     dg = pd.concat(pivotedCols + notPivotedCols, axis=1)
     dg.columns = list(columnsToPivot) + columnsNotToPivot
     return dg
+
 
 def _pivotSeries(s, start, end):
     return pd.concat([s.shift(i) for i in range(start, end, -1)], axis=1).apply(lambda x: list(x), axis=1)
