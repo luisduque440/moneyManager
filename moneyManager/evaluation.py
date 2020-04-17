f22b5ccdc7f29930247e4426b24b61250d01fc19
Luis Duque <lduque@indeed.com>
2020-04-17 13:45:11 -0500

    working on evaluation

--- moneyManager/evaluation.py
+++ moneyManager/evaluation.py
@@ -1,39 +1,39 @@
 from loadData.loadTimeSeries import getListOfAvailableStocks
 from stockModel.createTrainingDataSet import createTarget
 
-def _getIdealBuyTimes(stocks=None):
+def _loadExpectedBuy(stocks=None, futureLen):
     """ gets Times in which it is a good idea to buyAndKeep for 20 mins
     """
     stocks=getListOfAvailableStocks() if stocks==None else stocks
-    idealBuyTimes = {}
-    for s in stocks:
-        dB = createTarget(stock, numSamples=None, currentTime=None, futureLen):
-        idealBuyTimes[s] = set(dB[dB == True].index)
-    return idealBuyTimes
+    expectedBuy= {s:createTarget(stock, numSamples=None, currentTime=None, futureLen=futureLen) for s in stocks}
+    return expectedBuy
 
-
-idealBuyTimes = _getIdealBuyTimes()
-def evaluateSuggestions(moneyManagerSuggestions):
+def evaluateSuggestions(moneyManagerSuggestions, futureLen):
     """ document
     """
-    idealBuyTimes = _getIdealBuyTimes()
     buySuggestions = _getBuySuggestions(moneyManagerSuggestions)
-    correctBuySuggestions = {}
-    for s in stocks:
-        correctBuySuggestions[s] = [b for b in buySuggestions[s] if b in idealBuyTimes[s]]
+    stocks = [s for s in buySuggestions]
+    expectedBuy = _loadExpectedBuy(stocks, futureLen)
+    goodBuyTimes = {s: set(s.loc[s== True].index) for s in expectedBuy.values()}
 
-    buySuggestionsCount = sum([len(buySuggestions[s]) for s in stocks])
-    correctBuySuggestionsCount = sum([len(correctBuySuggestions[s]) for s in stocks])
-    print(buySuggestionsCount, correctBuySuggestionsCount)
+    totalCorrectBuySuggestions = sum([len([b for b in buySuggestions[s] if b in goodBuyTimes[s]]) for s in stocks])
+    totalBuySuggestions = sum([len(buySuggestions[s]) for s in stocks])
+    totalIdealBuyTimes = sum([len(goodBuyTimes[s]) for s in stocks])
+    totalPossibleMovements = sum([len(expectedBuy[s]) for s in stocks])
+
+    metrics = {
+        'totalCorrectBuySuggestions': totalCorrectBuySuggestions,
+        'totalBuySuggestions': totalBuySuggestions,
+        'totalIdealBuyTimes':totalIdealBuyTimes,
+        'totalPossibleMovements': totalPossibleMovements
+    }
+    return metrics
 
 
-# maybe money manager could do this already, makes no sense to have this separated.
-# also no point of stockModel also returning the label of the stock its recommending to buy, thats silly.
-# Maybe not.
 def _getBuySuggestions(moneyManagerSuggestions):
     """ document
     """
-    buyMoneyManagerSuggestions = [b for b in self.suggestions if b[0] == 'buyAndKeep20mins']
+    buyMoneyManagerSuggestions = [b for b in moneyManagerSuggestions if b[0] == 'buyAndKeep20mins']
     buySuggestions = {}
     for s in self.stocks:
         buySuggestions[s] = {b[2] for b in buyMoneyManagerSuggestions if b[1] == s}
